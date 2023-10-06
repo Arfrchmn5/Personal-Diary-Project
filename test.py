8b68f1f23fdd0a2ebf2ec2b5141eba7f59bc631a
Arfrchmn5 <ariefrh208@gmail.com>
2023-10-06 22:01:50 +0700

    First Commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-today  = datetime.now()
+today = datetime.now()
 
-date_time = now.strftime("%Y-%m-%d-%H-%M-%S")
+date_time = today.strftime('%Y-%m-%d-%H-%M-%S')
 
 print(date_time)
